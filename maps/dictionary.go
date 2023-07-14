ccd4483bf6390804cd5ed85f290bb91e36660023
Valerio Cofano <valecof@gmail.com>
2023-07-14 16:28:33 +0200

    feat: check error when updating a missing word

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -3,8 +3,9 @@ package dictionary
 type Dictionary map[string]string
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -37,6 +38,7 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, definition string) {
+func (d Dictionary) Update(word, definition string) error {
 	d[word] = definition
+	return nil
 }
