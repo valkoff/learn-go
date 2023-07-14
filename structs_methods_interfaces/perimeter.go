e1030fddc1f3dbc61751314870a7e932ece60b30
Valerio Cofano <valecof@gmail.com>
2023-07-14 12:59:17 +0200

    feat: rename perimeter package into geometry, new area requirement

--- structs_methods_interfaces/perimeter.go
+++ structs_methods_interfaces/perimeter.go
@@ -1,5 +1,9 @@
-package perimeter
+package geometry
 
 func Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
+}
+
+func Area(width float64, height float64) float64 {
+	return 0
 }
