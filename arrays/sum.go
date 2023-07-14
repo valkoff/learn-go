7bec61ce00439c64672d0d9ed5ace2d8d194ff89
Valerio Cofano <valecof@gmail.com>
2023-07-14 12:47:57 +0200

    fix: safely sum empty slices

--- arrays/sum.go
+++ arrays/sum.go
@@ -20,6 +20,10 @@ func SumAll(numbersToSum ...[]int) []int {
 func SumAllTails(numbersToSum ...[]int) []int {
 	var sums []int
 	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
 		tail := numbers[1:]
 		sums = append(sums, Sum(tail))
 	}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -27,12 +27,25 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	got := SumAllTails([]int{1, 2}, []int{0, 9})
-	want := []int{2, 9}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got '%d' want '%d'", got, want)
-	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got '%d' want '%d'", got, want)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got '%d' want '%d'", got, want)
+		}
+	})
+
 }
 
 func BenchmarkSum(b *testing.B) {
