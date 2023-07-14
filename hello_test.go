5630ca5d992b96cdca9aee2657d71f9ab71df0c9
Valerio Cofano <valecof@gmail.com>
2023-07-14 11:09:53 +0200

    feat: new requirements

--- hello_test.go
+++ hello_test.go
@@ -23,6 +23,13 @@ func TestHello(t *testing.T) {
 
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Gerard", "French")
+		want := "Bonjour, Gerard"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
