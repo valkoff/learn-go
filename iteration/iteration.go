5861b4bee267fe0895b8bea722f16917d4252c7b
Valerio Cofano <valecof@gmail.com>
2023-07-14 11:50:10 +0200

    feat: add example, use strings.Repeat

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,11 +1,7 @@
 package iteration
 
-const repeatCount = 5
+import "strings"
 
-func Repeat(character string) string {
-	var repeated string
-	for i := 0; i < repeatCount; i++ {
-		repeated += character
-	}
-	return repeated
+func Repeat(character string, repeatCount int) string {
+	return strings.Repeat(character, repeatCount)
 }
