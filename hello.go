042fbcff4dfd723e5da64d34309cfb52330771a4
Valerio Cofano <valecof@gmail.com>
2023-07-14 11:11:21 +0200

    feat: refactor to new requirements

--- hello.go
+++ hello.go
@@ -2,17 +2,28 @@ package main
 
 import "fmt"
 
+const defaultName = "World"
+
+const spanish = "Spanish"
+const french = "French"
+
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
-		name = "World"
+		name = defaultName
 	}
 
-	if language == "Spanish" {
+	if language == spanish {
 		return spanishHelloPrefix + name
 	}
+
+	if language == french {
+		return frenchHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
