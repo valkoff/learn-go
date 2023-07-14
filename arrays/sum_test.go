e68bfa6504c9bf036264a70edde5c4f029d60293
Valerio Cofano <valecof@gmail.com>
2023-07-14 12:01:34 +0200

    feat: allow any size of collection to be summed

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,12 +3,35 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got '%d' want '%d' given, '%v'", got, want, numbers)
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got '%d' want '%d' given, '%v'", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got '%d' want '%d' given, '%v'", got, want, numbers)
+		}
+	})
+
+}
+
+func BenchmarkSum(b *testing.B) {
+	numbers := []int{1, 2, 3, 4, 5}
+
+	for i := 0; i < b.N; i++ {
+		Sum(numbers)
 	}
 }
