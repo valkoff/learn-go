20ed214a27ed58cfeaea4fb46e26fd3924ce857a
Valerio Cofano <valecof@gmail.com>
2023-07-14 13:52:21 +0200

    feat: area function for rectangle and circle

---
+++ structs_methods_interfaces/circle.go
@@ -0,0 +1,9 @@
+package geometry
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 0
+}
--- structs_methods_interfaces/geometry.go
+++ structs_methods_interfaces/geometry.go
@@ -1,9 +1,9 @@
 package geometry
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
