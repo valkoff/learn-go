0d092e980988107bd22a95080d474406248ef05a
Valerio Cofano <valecof@gmail.com>
2023-07-14 13:59:01 +0200

    feat: refactor with shape interface

--- structs_methods_interfaces/geometry.go
+++ structs_methods_interfaces/geometry.go
@@ -1,9 +1,9 @@
 package geometry
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Shape interface {
+	Area() float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
