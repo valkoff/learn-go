8fab182003e41e796c704a725d211bcb7450eba6
Valerio Cofano <valecof@gmail.com>
2023-07-17 16:46:52 +0200

    feat: first implementation

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -1,5 +1,15 @@
 package main
 
-import "bytes"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
-func Countdown(out *bytes.Buffer) {}
+func Countdown(out io.Writer) {
+	fmt.Fprint(out, "3")
+}
+
+func main() {
+	Countdown(os.Stdout)
+}
