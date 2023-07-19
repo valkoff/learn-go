f43cbb8d1c235e9f5a67d915eac4b042d0a56b90
Valerio Cofano <valecof@gmail.com>
2023-07-19 15:55:23 +0200

    feat: minor refactor

--- select/racer_test.go
+++ select/racer_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestRacer(t *testing.T) {
 
-	t.Run("racer returns the fastest URL", func(t *testing.T) {
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
 		slowServer := mockDelayedServer(20 * time.Millisecond)
 		fastServer := mockDelayedServer(0 * time.Millisecond)
 
@@ -27,19 +27,18 @@ func TestRacer(t *testing.T) {
 		}
 	})
 
-	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
-		serverA := mockDelayedServer(11 * time.Second)
-		serverB := mockDelayedServer(12 * time.Second)
+	t.Run("returns an error if a server doesn't respond within timeout", func(t *testing.T) {
+		server := mockDelayedServer(25 * time.Millisecond)
 
-		defer serverA.Close()
-		defer serverB.Close()
+		defer server.Close()
 
-		_, err := Racer(serverA.URL, serverB.URL)
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
 
 		if err == nil {
 			t.Error("expected an error but didn't get one")
 		}
 	})
+
 }
 
 func mockDelayedServer(delay time.Duration) *httptest.Server {
