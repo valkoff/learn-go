28da8987791439a20dcd0e046326c58642728c10
Valerio Cofano <valecof@gmail.com>
2023-07-19 15:56:18 +0200

    6 select (#27)
    * feat: requirement implementation
    
    * feat: logic and mocked http testing
    
    * feat: refactoring
    
    * feat: test error when server doesn't respond within 10s
    
    * feat: error logic in Racer
    
    * feat: minor refactor

---
+++ select/racer_test.go
@@ -0,0 +1,49 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := mockDelayedServer(20 * time.Millisecond)
+		fastServer := mockDelayedServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within timeout", func(t *testing.T) {
+		server := mockDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
+
+}
+
+func mockDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
