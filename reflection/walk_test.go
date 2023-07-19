488a8dd300ba128db907bc3e7468b6fcfce06aab
Valerio Cofano <valecof@gmail.com>
2023-07-19 16:55:04 +0200

    feat: function

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -131,6 +131,23 @@ func TestWalk(t *testing.T) {
 		}
 	})
 
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{1991, "Rome"}, Profile{2019, "Milan"}
+		}
+
+		var got []string
+		want := []string{"Rome", "Milan"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
 }
 
 func assertContains(t testing.TB, haystack []string, needle string) {
