74371bc1a950c3b0f3e7b4aa6338c003104d37d8
Valerio Cofano <valecof@gmail.com>
2023-07-19 12:45:35 +0200

    4 mocking (#25)
    * feat: add requirement
    
    * feat: first implementation
    
    * feat: write 3, 2, 1, Go!
    
    * feat: mock sleeper
    
    * fix: ensure sleep before every print
    
    * feat: extend sleeper to be configurable
    
    * fix: refactor by removing default sleeper

---
+++ mocking/countdown.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{duration: 2 * time.Second, sleep: time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
