60042b25f250f518a5dc1ea126383db352915f19
Valerio Cofano <valecof@gmail.com>
2023-07-19 15:34:34 +0200

    feat: test error when server doesn't respond within 10s

--- select/racer_test.go
+++ select/racer_test.go
@@ -9,21 +9,37 @@ import (
 
 func TestRacer(t *testing.T) {
 
-	slowServer := mockDelayedServer(20 * time.Millisecond)
-	defer slowServer.Close()
+	t.Run("racer returns the fastest URL", func(t *testing.T) {
+		slowServer := mockDelayedServer(20 * time.Millisecond)
+		fastServer := mockDelayedServer(0 * time.Millisecond)
 
-	fastServer := mockDelayedServer(0 * time.Millisecond)
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := mockDelayedServer(11 * time.Second)
+		serverB := mockDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }
 
 func mockDelayedServer(delay time.Duration) *httptest.Server {
